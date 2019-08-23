0abebec0a8b912d3f02034198b2a779603b9e695
Louiskoyio <koyiolouis@gmail.com>
2019-08-23 12:34:58 +0300

    Added Results Component Logic

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  users:User[];
 
-  constructor() { }
+  constructor(public searchService:SearchService) { }
 
   ngOnInit() {
+    this.searchUser("Louiskoyio");
   }
+
+
+	searchUser(userQuery){
+    this.searchService.searchUsers(userQuery).then(
+      ()=>{
+        this.users=this.searchService.users;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+    }
 
 }
