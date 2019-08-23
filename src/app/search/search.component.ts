c98e434f883e4e1185dd4248eee05998a953a9f0
Louiskoyio <koyiolouis@gmail.com>
2019-08-23 12:19:25 +0300

    Added Search Form Logic

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  @Output() emitSearch = new EventEmitter<any>()
 
+  userQuery:string;
   constructor() { }
+
+  search(){
+    this.emitSearch.emit(this.userQuery);
+  }
 
   ngOnInit() {
   }
