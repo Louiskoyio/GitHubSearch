71f72772bd57eb7ef894eb893d0802f588d8fc84
Louiskoyio <koyiolouis@gmail.com>
2019-08-23 11:46:57 +0300

    Layout Prepared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
